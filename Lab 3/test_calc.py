bb1809e9128a3676e44509250fa5373a2a217bb1
Jonathan Woo <jonathan.ck.woo@gmail.com>
2020-09-30 13:42:46 -0400

    Handled float values.

--- Lab 3/test_calc.py
+++ Lab 3/test_calc.py
@@ -14,7 +14,7 @@ if __name__ == '__main__':
     
     #Problem 2: Sums of Cubes
     total = 0
-    n = -2
+    n = 2.3
 
     def sum_of_cubes(n):
         '''Calculates and returns the sum of cubes from 1 to
@@ -24,8 +24,8 @@ if __name__ == '__main__':
         global total
         total = 0
         
-        if n <= 0:
-            print("Value is too low. Cannot find sum of cubes. Choose a value >= 1.")
+        if n <= 0 or n % 1 != 0:
+            print("Value invalid. Cannot find sum of cubes. Choose an integer >= 1.")
             return None
 
         for i in range (1, n + 1):
@@ -36,8 +36,8 @@ if __name__ == '__main__':
         '''Calculates and returns the sum of cubes from 1 to n.
         Uses a formula.
         '''
-        if n <= 0:
-            print("Value is too low. Cannot find sum of cubes. Choose a value >= 1.")
+        if n <= 0 or n % 1 != 0:
+            print("Value invalid. Cannot find sum of cubes. Choose an integer >= 1.")
             return None
 
         return (n * (n + 1) / 2) ** 2
@@ -48,8 +48,8 @@ if __name__ == '__main__':
         If they are, this returns True, otherwise, it returns
         False.
         '''
-        if n <= 0:
-            print("Value is too low. Cannot find sum of cubes. Choose a value >= 1.")
+        if n <= 0 or n % 1 != 0:
+            print("Value invalid. Choose an integer >= 1.")
             return None
 
         return (sum_of_cubes(n) == sum_of_cubes_formula(n))
@@ -59,8 +59,8 @@ if __name__ == '__main__':
         sum_of_cubes_formula() are equal for all values 1
         to n.
         '''
-        if n <= 0:
-            print("Value is too low. Cannot find sum of cubes. Choose a value >= 1.")
+        if n <= 0 or n % 1 != 0:
+            print("Value invalid. Choose an integer >= 1.")
             return None
 
         for i in range(1, n + 1):
@@ -68,20 +68,16 @@ if __name__ == '__main__':
                 return False
         return True
 
-    print(check_sums_up_to_n(n))
-
     #Problem 3: Pi
     def lebiniz_formula(n):
         '''Calculates and returns the lebiniz formula from
         0 to n
         '''
-        if n <= 0:
-            print("Value is too low. Cannot find sum of cubes. Choose a value >= 1.")
+        if n <= 0 or n % 1 != 0:
+            print("Value invalid. Cannot calculate pi. Choose an integer >= 1.")
             return None
 
         lebiniz_total = 0
         for i in range (0, n + 1):
             lebiniz_total += ((-1) ** i) / (2 * i + 1)
-        return lebiniz_total * 4
-
-    print (lebiniz_formula(n))
+        return lebiniz_total * 4    
