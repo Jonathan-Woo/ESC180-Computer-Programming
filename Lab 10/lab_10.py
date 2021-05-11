ab584b0c16456e2c08f31b9260764577182f355f
Jonathan Woo <jonathan.ck.woo@gmail.com>
2021-05-10 22:32:06 -0400

    update

--- Lab 10/lab_10.py
+++ Lab 10/lab_10.py
@@ -28,30 +28,14 @@ def interleave(L1, L2):
 
 # Problem 3
 
-def reverse_loop(L):
+def reverse_loop(L, n = 0):
     ''' Reverses a list'''
+    if n == len(L)//2:
+        return L
+    else:
+        L[n], L[-1 - n] = L[-1 - n], L[n]
+        return reverse_loop(L, n + 1)
     
-    # L = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    # L[-1-i] = 9, 8, 7, 6
-    
-    # for i in range(len(L)//2):
-        # L[i], L[-1-i] = L[-1-i], L[i]
-    
-    length = len(L)
-    mid = len(L) // 2
-    ans = []
-    
-    if length == 1:
-        ans.append(L[0])
-        return ans
-    
-    L[length - 1], L[-1-(length - 1)] = L[-1-(length - 1)], L[length - 1]
-    
-    return reverse_loop(L[:mid-1]) + ans
-        
-        
-# Problem 4
-
 def zigzag1(L):
     '''Prints a list L in the order: L[n//2] L[n//2-1] L[n//2+1] 
     L[n//2-2]...
@@ -60,17 +44,19 @@ def zigzag1(L):
     '''
     #base case
     if len(L) == 0:
-        print('')
+        print('', sep = "")
     elif len(L) == 1:
-        print(L[0], end = "")
+        print(L[0], end = "", sep = "")
         
-    #even case
-    elif len(L) % 2 != 0:
-        print(L[len(L)//2])
-        zigzag1(L[0:len(L)//2] + L[len(L)//2 + 1:])
     #odd case
+    elif len(L) % 2 != 0:
+        print(L[len(L)//2], end = "", sep = "")
+        zigzag1(L[0:len(L)//2] + L[len(L)//2 + 1:])
+        
+        [1,2,4,5]
+    #even case
     else:
-        print(L[len(L)//2], L[len(L)//2 - 1], end = "\n")
+        print(L[len(L)//2], L[len(L)//2 - 1], end = "", sep = "")
         zigzag1(L[0:len(L)//2 - 1] + L[len(L)//2+1:])
                 
 # Problem 5
@@ -87,7 +73,7 @@ def is_balanced(s):
         loc_start = s.index('(')
         loc_end = s.index(')')
         if loc_start < loc_end:
-            s = s[loc_start + 1:loc_end] 
+            s = s[loc_start + 1:loc_end] + s[loc_end + 1:] 
             return is_balanced(s)
         else:
             return False
